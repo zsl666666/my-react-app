b487080bb7d9917a375c796b1e67d79af844b3d2
zhoushanlin <zhoushanlin@inke.cn>
2020-12-23 12:39:22 +0800

    init

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,6 @@ const token = localStorage.getItem('token')
 const instance = axios.create({
     timeout: 5000
 })
-
 // 设置post请求头
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
