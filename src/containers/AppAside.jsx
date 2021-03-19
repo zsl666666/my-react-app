6fd324e9dc26900de1e38fa1a788b39f4792eeb8
zsl666666 <1484968273@qq.com>
2021-03-19 16:08:06 +0800

    logo路径修改

--- src/containers/AppAside.jsx
+++ src/containers/AppAside.jsx
@@ -12,11 +12,7 @@ const AppAside = props => {
       <div className='logo'>
         <a rel='noopener noreferrer' href='https://github.com/ltadpoles' target='_blank'>
           {/* <Icon type='github' style={{ fontSize: '3.8rem', color: '#fff' }} /> */}
-          <img
-            alt=''
-            src='https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwx4.sinaimg.cn%2Fmw690%2F66b6e1c4ly1fe74swo37rj20le0ladg5.jpg&refer=http%3A%2F%2Fwx4.sinaimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1613120543&t=a21446fad6a4dab01e121e4140da3d56'
-            style={{ width: '100%', height: '100%' }}
-          />
+          <img alt='' src={require('@/assets/images/user.jpg')} style={{ width: '100%', height: '100%' }} />
         </a>
       </div>
       <CustomMenu menu={menu}></CustomMenu>
